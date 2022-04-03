e66d44f4a23507920ed382ed11ca0c860760aacb
HanzBOTZ <100819969+HANZBOTZGZ@users.noreply.github.com>
2022-04-03 11:24:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'Nezuko'
+global.owner = ['6288215110787','0']
+global.packname = 'ʜᴀɴᴢ ʙᴏᴛᴢ'
+global.author = 'ʜᴀɴᴢᴏғᴄ'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'ʜᴀɴᴢ ᴏғᴄ'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
